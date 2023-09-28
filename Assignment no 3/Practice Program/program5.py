729a221efee49759f182e0e599e466fc83ed2342
Aniket  varkhade <118761872+aniket-oo4@users.noreply.github.com>
2023-09-28 13:12:50 +0530

    Update program5.py
    biggest mistake changed

--- Assignment no 3/Practice Program/program5.py
+++ Assignment no 3/Practice Program/program5.py
@@ -1,4 +1,6 @@
-my_dictonary = {0: 10, 1: 20}
+my_dictonary = {0: 10, 1: 20} // dictionery spelling was incorrect correct it 🙄🙄🙄
 print(my_dictonary)
 my_dictonary[2] = 30
 print(my_dictonary)
+my_dictionary={0:"tu",1:"veda",2:"ahes"}
+print(my_dictionary)
