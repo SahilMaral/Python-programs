1057e240ce61ea5a02360648b14bcf12f12826d7
Sahil Maral <125784693+SahilMaral@users.noreply.github.com>
2023-09-29 17:56:24 +0530

    Delete Assignment no 3/Practice Program/program5.py

--- Assignment no 3/Practice Program/program5.py
+++
@@ -1,6 +0,0 @@
-my_dictonary = {0: 10, 1: 20} // dictionery spelling was incorrect correct it 🙄🙄🙄
-print(my_dictonary)
-my_dictonary[2] = 30
-print(my_dictonary)
-my_dictionary={0:"tu",1:"veda",2:"ahes"}
-print(my_dictionary)
